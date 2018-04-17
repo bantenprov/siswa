c240af6a094f151bc8d8ea70e07db3e2290f3d66
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2018-04-17 14:14:36 +0700

    Pretify migration

--- src/database/migrations/2018_02_07_084934_create_siswas_table.php
+++ src/database/migrations/2018_02_07_084934_create_siswas_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Carbon\Carbon;
 
 class CreateSiswasTable extends Migration
 {
@@ -14,27 +13,26 @@ class CreateSiswasTable extends Migration
 	public function up()
 	{
 		Schema::create('siswas', function (Blueprint $table) {
-			 $table->increments('id');
-			 $table->bigInteger('nomor_un');
-			 //$table->bigInteger('pendaftaran_id')->unique();
-			 $table->bigInteger('nik');
-			 $table->string('nama_siswa');
-			 $table->bigInteger('no_kk');
-			 $table->text('alamat_kk');
-			 $table->string('province_id')->nullable();
-			 $table->string('city_id')->nullable();
-			 $table->string('district_id')->nullable();
-			 $table->string('village_id')->nullable();
-			 $table->string('tempat_lahir');
-			 $table->date('tgl_lahir');
-			 $table->string('jenis_kelamin');
-			 $table->string('agama');
-			 $table->bigInteger('nisn');	
-			 $table->string('tahun_lulus');
-			 $table->integer('sekolah_id')->nullable();
-			 $table->integer('user_id');
-			 $table->timestamps();
-			 $table->softDeletes();
+			$table->increments('id');
+			$table->bigInteger('nomor_un');
+			$table->bigInteger('nik');
+			$table->string('nama_siswa');
+			$table->bigInteger('no_kk');
+			$table->text('alamat_kk');
+			$table->string('province_id')->nullable();
+			$table->string('city_id')->nullable();
+			$table->string('district_id')->nullable();
+			$table->string('village_id')->nullable();
+			$table->string('tempat_lahir');
+			$table->date('tgl_lahir');
+			$table->string('jenis_kelamin');
+			$table->string('agama');
+			$table->bigInteger('nisn');
+			$table->string('tahun_lulus');
+			$table->integer('sekolah_id')->nullable();
+			$table->integer('user_id');
+			$table->timestamps();
+			$table->softDeletes();
 		});
 	}
 

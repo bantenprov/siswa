893910bf5b58cfbbeef7c474ced9e34c8ad709fd
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2018-04-17 17:29:58 +0700

    Add message and status respon on index

--- src/Http/Controllers/SiswaController.php
+++ src/Http/Controllers/SiswaController.php
@@ -79,7 +79,10 @@ class SiswaController extends Controller
         }
 
         $perPage    = request()->has('per_page') ? (int) request()->per_page : null;
+
         $response   = $query->with(['province', 'city', 'district', 'village', 'sekolah', 'user'])->paginate($perPage);
+        $response['message']    = 'Loaded';
+        $response['status']     = true;
 
         return response()->json($response)
             ->header('Access-Control-Allow-Origin', '*')

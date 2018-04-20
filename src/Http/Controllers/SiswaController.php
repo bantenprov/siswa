5185c539eae765b6449c15798b864c22dd15ce78
fadika06 <fadikariansya06@gmail.com>
2018-04-21 01:44:02 +0700

    update siswa

---
+++ src/Http/Controllers/SiswaController.php
@@ -0,0 +1,413 @@
+<?php
+
+namespace Bantenprov\Siswa\Http\Controllers;
+
+/* Require */
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Bantenprov\Siswa\Facades\SiswaFacade;
+
+/* Models */
+use Bantenprov\Siswa\Models\Bantenprov\Siswa\Siswa;
+use Laravolt\Indonesia\Models\Province;
+use Laravolt\Indonesia\Models\City;
+use Laravolt\Indonesia\Models\District;
+use Laravolt\Indonesia\Models\Village;
+use Bantenprov\Sekolah\Models\Bantenprov\Sekolah\Sekolah;
+use Bantenprov\Sekolah\Models\Bantenprov\Sekolah\ProdiSekolah;
+use App\User;
+
+/* Etc */
+use Validator;
+use Auth;
+
+/**
+ * The SiswaController class.
+ *
+ * @package Bantenprov\Siswa
+ * @author  bantenprov <developer.bantenprov@gmail.com>
+ */
+class SiswaController extends Controller
+{
+    protected $siswa;
+    protected $province;
+    protected $city;
+    protected $district;
+    protected $village;
+    protected $sekolah;
+    protected $prodi_sekolah;
+    protected $user;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->siswa            = new Siswa;
+        $this->province         = new Province;
+        $this->city             = new City;
+        $this->district         = new District;
+        $this->village          = new Village;
+        $this->sekolah          = new Sekolah;
+        $this->prodi_sekolah    = new ProdiSekolah;
+        $this->user             = new User;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if (request()->has('sort')) {
+            list($sortCol, $sortDir) = explode('|', request()->sort);
+
+            $query = $this->siswa->orderBy($sortCol, $sortDir);
+        } else {
+            $query = $this->siswa->orderBy('id', 'asc');
+        }
+
+        if ($request->exists('filter')) {
+            $query->where(function($q) use($request) {
+                $value = "%{$request->filter}%";
+
+                $q->where('nomor_un', 'like', $value)
+                    ->orWhere('nik', 'like', $value)
+                    ->orWhere('nama_siswa', 'like', $value)
+                    ->orWhere('no_kk', 'like', $value)
+                    ->orWhere('nisn', 'like', $value);
+            });
+        }
+
+        $perPage    = request()->has('per_page') ? (int) request()->per_page : null;
+
+        $response   = $query->with(['province', 'city', 'district', 'village', 'sekolah', 'prodi_sekolah', 'user'])->paginate($perPage);
+
+        foreach($response as $siswa){
+            if (isset($siswa->prodi_sekolah->program_keahlian)) {
+                $siswa->prodi_sekolah->program_keahlian;
+            }
+        }
+
+        return response()->json($response)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function get()
+    {
+        $siswas = $this->siswa->with(['province', 'city', 'district', 'village', 'sekolah', 'prodi_sekolah', 'user'])->get();
+
+        foreach($siswas as $siswa){
+            array_set($siswa, 'label', $siswa->nama);
+
+            if (isset($siswa->prodi_sekolah->program_keahlian)) {
+                $siswa->prodi_sekolah->program_keahlian;
+            }
+        }
+
+        $response['siswas']   = $siswas;
+        $response['error']      = false;
+        $response['message']    = 'Success';
+        $response['status']     = true;
+
+        return response()->json($response);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $user_id        = isset(Auth::User()->id) ? Auth::User()->id : null;
+        $siswa          = $this->siswa->getAttributes();
+        $provinces      = $this->province->getAttributes();
+        $cities         = $this->city->getAttributes();
+        $districts      = $this->district->getAttributes();
+        $villages       = $this->village->getAttributes();
+        $sekolahs       = $this->sekolah->getAttributes();
+        $prodi_sekolahs = $this->prodi_sekolah->getAttributes();
+        $users          = $this->user->getAttributes();
+        $users_special  = $this->user->all();
+        $users_standar  = $this->user->findOrFail($user_id);
+        $current_user   = Auth::User();
+
+        foreach($provinces as $province){
+            array_set($province, 'label', $province->name);
+        }
+
+        foreach($cities as $city){
+            array_set($city, 'label', $city->name);
+        }
+
+        foreach($districts as $district){
+            array_set($district, 'label', $district->name);
+        }
+
+        foreach($villages as $village){
+            array_set($village, 'label', $village->name);
+        }
+
+        foreach($sekolahs as $sekolah){
+            array_set($sekolah, 'label', $sekolah->nama);
+        }
+
+        foreach($prodi_sekolahs as $prodi_sekolah){
+            array_set($prodi_sekolah, 'label', $prodi_sekolah->keterangan);
+        }
+
+        $role_check = Auth::User()->hasRole(['superadministrator','administrator']);
+
+        if($role_check){
+            $user_special = true;
+
+            foreach($users_special as $user){
+                array_set($user, 'label', $user->name);
+            }
+
+            $users = $users_special;
+        }else{
+            $user_special = false;
+
+            array_set($users_standar, 'label', $users_standar->name);
+
+            $users = $users_standar;
+        }
+
+        array_set($current_user, 'label', $current_user->name);
+
+        $response['siswa']          = $siswa;
+        $response['provinces']      = $provinces;
+        $response['cities']         = $cities;
+        $response['districts']      = $districts;
+        $response['villages']       = $villages;
+        $response['sekolahs']       = $sekolahs;
+        $response['prodi_sekolahs'] = $prodi_sekolahs;
+        $response['users']          = $users;
+        $response['user_special']   = $user_special;
+        $response['current_user']   = $current_user;
+        $response['error']          = false;
+        $response['message']        = 'Success';
+        $response['status']         = true;
+
+        return response()->json($response);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $siswa      = $this->siswa;
+        $validator  = Validator::make($request->all(), [
+            'nomor_un'          => "required|max:255|unique:{$this->siswa->getTable()},nomor_un,NULL,id,deleted_at,NULL",
+            'nik'               => "required|digits:16|unique:{$this->siswa->getTable()},nik,NULL,id,deleted_at,NULL",
+            'nama_siswa'        => 'required|max:255',
+            'no_kk'             => "required|digits:16|unique:{$this->siswa->getTable()},no_kk,NULL,id,deleted_at,NULL",
+            'alamat_kk'         => 'required|max:255',
+            'province_id'       => "required|exists:{$this->province->getTable()},id",
+            'city_id'           => "required|exists:{$this->city->getTable()},id",
+            'district_id'       => "required|exists:{$this->district->getTable()},id",
+            'village_id'        => "required|exists:{$this->village->getTable()},id",
+            'tempat_lahir'      => 'required|max:255',
+            'tgl_lahir'         => 'required|date',
+            'jenis_kelamin'     => 'required|max:255',
+            'agama'             => 'required|max:255',
+            'nisn'              => 'required|digits:255',
+            'tahun_lulus'       => 'required|date_format:Y',
+            'sekolah_id'        => "required|exists:{$this->sekolah->getTable()},id",
+            'prodi_sekolah_id'  => "required|exists:{$this->prodi_sekolah->getTable()},id",
+            'user_id'           => "required|exists:{$this->user->getTable()},id",
+        ]);
+
+        if ($validator->fails()) {
+            $error      = true;
+            $message    = $validator->errors()->first();
+        } else {
+            $siswa->user_id             = $request->input('user_id');
+            $siswa->nomor_un            = $request->input('nomor_un');
+            $siswa->nik                 = $request->input('nik');
+            $siswa->nama_siswa          = $request->input('nama_siswa');
+            $siswa->no_kk               = $request->input('no_kk');
+            $siswa->alamat_kk           = $request->input('alamat_kk');
+            $siswa->province_id         = $request->input('province_id');
+            $siswa->city_id             = $request->input('city_id');
+            $siswa->district_id         = $request->input('district_id');
+            $siswa->village_id          = $request->input('village_id');
+            $siswa->tempat_lahir        = $request->input('tempat_lahir');
+            $siswa->tgl_lahir           = $request->input('tgl_lahir');
+            $siswa->jenis_kelamin       = $request->input('jenis_kelamin');
+            $siswa->agama               = $request->input('agama');
+            $siswa->nisn                = $request->input('nisn');
+            $siswa->sekolah_id          = $request->input('sekolah_id');
+            $siswa->prodi_sekolah_id    = $request->input('prodi_sekolah_id');
+            $siswa->tahun_lulus         = $request->input('tahun_lulus');
+            $siswa->save();
+
+            $error      = false;
+            $message    = 'Success';
+        }
+
+        $response['siswa']      = $siswa;
+        $response['error']      = $error;
+        $response['message']    = $message;
+        $response['status']     = true;
+
+        return response()->json($response);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $siswa = $this->siswa->with(['province', 'city', 'district', 'village', 'sekolah', 'prodi_sekolah', 'user'])->findOrFail($id);
+
+        if (isset($siswa->prodi_sekolah->program_keahlian)) {
+            $siswa->prodi_sekolah->program_keahlian;
+        }
+
+        $response['siswa']      = $siswa;
+        $response['error']      = false;
+        $response['message']    = 'Success';
+        $response['status']     = true;
+
+        return response()->json($response);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $siswa = $this->siswa->with(['province', 'city', 'district', 'village', 'sekolah', 'prodi_sekolah', 'user'])->findOrFail($id);
+
+        $response['siswa']      = $siswa;
+        $response['error']      = false;
+        $response['message']    = 'Success';
+        $response['status']     = true;
+
+        return response()->json($response);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $response = array();
+        $message  = array();
+        $siswa = $this->siswa->findOrFail($id);
+
+            $validator = Validator::make($request->all(), [
+                'user_id'       => 'required|unique:siswas,user_id,'.$id,
+                'nomor_un'      => 'required|unique:siswas,nomor_un,'.$id,
+                'nik'           => 'required|unique:siswas,nik,'.$id,
+                'nama_siswa'    => 'required',
+                'alamat_kk'     => 'required',
+                'tempat_lahir'  => 'required',
+                'tgl_lahir'     => 'required',
+                'jenis_kelamin' => 'required',
+                'agama'         => 'required',
+                'nisn'          => 'required',
+                'tahun_lulus'   => 'required',
+
+                ]);
+
+                if($validator->fails()){
+
+                    foreach($validator->messages()->getMessages() as $key => $error){
+                        foreach($error AS $error_get) {
+                            array_push($message, $error_get);
+                        }
+                    }
+
+
+                    $check_user     = $this->siswa->where('id','!=', $id)->where('user_id', $request->user_id);
+                    $check_nomor_un = $this->siswa->where('id','!=', $id)->where('nomor_un', $request->nomor_un);
+                    $check_nik      = $this->siswa->where('id','!=', $id)->where('nik', $request->nik);
+
+                    if($check_nomor_un->count() > 0 || $check_user->count() > 0 || $check_nik->count() > 0){
+                        $response['message'] = implode("\n",$message);
+
+                    }else{
+                        $siswa->user_id = $request->input('user_id');
+                        $siswa->nomor_un = $request->input('nomor_un');
+                        $siswa->nik = $request->input('nik');
+                        $siswa->nama_siswa = $request->input('nama_siswa');
+                        $siswa->alamat_kk = $request->input('alamat_kk');
+                        $siswa->tempat_lahir = $request->input('tempat_lahir');
+                        $siswa->tgl_lahir = $request->input('tgl_lahir');
+                        $siswa->jenis_kelamin = $request->input('jenis_kelamin');
+                        $siswa->agama = $request->input('agama');
+                        $siswa->nisn = $request->input('nisn');
+                        $siswa->tahun_lulus = $request->input('tahun_lulus');
+                        $siswa->save();
+                        $response['message'] = 'success';
+                    }
+             }else{
+                    $siswa->user_id = $request->input('user_id');
+                    $siswa->nomor_un = $request->input('nomor_un');
+                    $siswa->nik = $request->input('nik');
+                    $siswa->nama_siswa = $request->input('nama_siswa');
+                    $siswa->alamat_kk = $request->input('alamat_kk');
+                    $siswa->tempat_lahir = $request->input('tempat_lahir');
+                    $siswa->tgl_lahir = $request->input('tgl_lahir');
+                    $siswa->jenis_kelamin = $request->input('jenis_kelamin');
+                    $siswa->agama = $request->input('agama');
+                    $siswa->nisn = $request->input('nisn');
+                    $siswa->tahun_lulus = $request->input('tahun_lulus');
+                    $siswa->save();
+                    $response['message'] = 'success';
+        }
+
+        $response['status'] = true;
+        return response()->json($response);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $siswa = $this->siswa->findOrFail($id);
+
+        if ($siswa->delete()) {
+            $response['message']    = 'Success';
+            $response['success']    = true;
+            $response['status']     = true;
+        } else {
+            $response['message']    = 'Failed';
+            $response['success']    = false;
+            $response['status']     = false;
+        }
+
+        return json_encode($response);
+    }
+}
---
+++ src/resources/assets/js/components/bantenprov/siswa/Siswa.edit.vue
@@ -0,0 +1,322 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <i class="fa fa-table" aria-hidden="true"></i> Edit Data Siswa
+
+      <ul class="nav nav-pills card-header-pills pull-right">
+        <li class="nav-item">
+          <button class="btn btn-primary btn-sm" role="button" @click="back">
+            <i class="fa fa-arrow-left" aria-hidden="true"></i>
+          </button>
+        </li>
+      </ul>
+    </div>
+
+    <div class="card-body">
+      <vue-form class="form-horizontal form-validation" :state="state" @submit.prevent="onSubmit">
+
+        <div class="form-row mt-4">
+          <div class="col-md">
+            <validate tag="div">
+              <label for="nomor_un">Nomor UN</label>
+              <input class="form-control" v-model="model.nomor_un" name="nomor_un" type="text" placeholder="Nomor UN">
+
+              <field-messages name="nomor_un" show="$invalid && $submitted" class="text-danger">
+                <small class="form-text text-success">Looks good!</small>
+              </field-messages>
+            </validate>
+          </div>
+        </div>
+
+        <div class="form-row mt-4">
+          <div class="col-md">
+            <validate tag="div">
+              <label for="nik">NIK</label>
+              <input class="form-control" v-model="model.nik" name="nik" type="text" placeholder="NIK">
+
+              <field-messages name="nik" show="$invalid && $submitted" class="text-danger">
+                <small class="form-text text-success">Looks good!</small>
+              </field-messages>
+            </validate>
+          </div>
+        </div>
+
+        <div class="form-row mt-4">
+          <div class="col-md">
+            <validate tag="div">
+              <label for="nama_siswa">Nama Siswa</label>
+              <input class="form-control" v-model="model.nama_siswa" name="nama_siswa" type="text" placeholder="Nama Siswa">
+
+              <field-messages name="nama_siswa" show="$invalid && $submitted" class="text-danger">
+                <small class="form-text text-success">Looks good!</small>
+              </field-messages>
+            </validate>
+          </div>
+        </div>
+
+        <div class="form-row mt-4">
+          <div class="col-md">
+            <validate tag="div">
+              <label for="alamat_kk">Alamat KK</label>
+              <input class="form-control" v-model="model.alamat_kk" name="alamat_kk" type="text" placeholder="Alamat KK">
+
+              <field-messages name="alamat_kk" show="$invalid && $submitted" class="text-danger">
+                <small class="form-text text-success">Looks good!</small>
+              </field-messages>
+            </validate>
+          </div>
+        </div>
+
+        <div class="form-row mt-4">
+          <div class="col-md">
+            <validate tag="div">
+              <label for="tempat_lahir">Tempat Lahir</label>
+              <input class="form-control" v-model="model.tempat_lahir" name="tempat_lahir" type="text" placeholder="Tempat Lahir">
+
+              <field-messages name="tempat_lahir" show="$invalid && $submitted" class="text-danger">
+                <small class="form-text text-success">Looks good!</small>
+              </field-messages>
+            </validate>
+          </div>
+        </div>
+
+        <div class="form-row mt-4">
+          <div class="col-md">
+            <validate tag="div">
+              <label for="tgl_lahir">Tanggal Lahir</label>
+              <input class="form-control" v-model="model.tgl_lahir" name="tgl_lahir" type="text" placeholder="Tanggal Lahir">
+
+              <field-messages name="tgl_lahir" show="$invalid && $submitted" class="text-danger">
+                <small class="form-text text-success">Looks good!</small>
+              </field-messages>
+            </validate>
+          </div>
+        </div>
+
+        <div class="form-row mt-4">
+          <div class="col-md">
+            <validate tag="div">
+            <label for="jenis_kelamin">Jenis Kelamin</label>
+            <v-select v-model="model.jenis_kelamin" :options="jenis_kelamin" class="mb-4"></v-select>
+
+            <field-messages name="jenis_kelamin" show="$invalid && $submitted" class="text-danger">
+              <small class="form-text text-success">Looks good!</small>
+              <small class="form-text text-danger" slot="required">Jenis Kelamin is a required field</small>
+            </field-messages>
+            </validate>
+          </div>
+        </div>
+
+        <div class="form-row mt-4">
+          <div class="col-md">
+            <validate tag="div">
+            <label for="agama">Agama</label>
+            <v-select v-model="model.agama" :options="agama" class="mb-4"></v-select>
+
+            <field-messages name="agama" show="$invalid && $submitted" class="text-danger">
+              <small class="form-text text-success">Looks good!</small>
+              <small class="form-text text-danger" slot="required">Agama is a required field</small>
+            </field-messages>
+            </validate>
+          </div>
+        </div>
+
+        <div class="form-row mt-4">
+          <div class="col-md">
+            <validate tag="div">
+              <label for="nisn">NISN</label>
+              <input class="form-control" v-model="model.nisn" name="nisn" type="text" placeholder="NISN">
+
+              <field-messages name="nisn" show="$invalid && $submitted" class="text-danger">
+                <small class="form-text text-success">Looks good!</small>
+              </field-messages>
+            </validate>
+          </div>
+        </div>
+
+        <div class="form-row mt-4">
+          <div class="col-md">
+            <validate tag="div">
+              <label for="tahun_lulus">Tahun Lulus</label>
+              <input class="form-control" v-model="model.tahun_lulus" name="tahun_lulus" type="text" placeholder="Tahun Lulus">
+
+              <field-messages name="tahun_lulus" show="$invalid && $submitted" class="text-danger">
+                <small class="form-text text-success">Looks good!</small>
+              </field-messages>
+            </validate>
+          </div>
+        </div>
+
+        <div class="form-row mt-4">
+          <div class="col-md">
+            <validate tag="div">
+            <label for="user_id">Username</label>
+            <v-select name="user_id" v-model="model.user" :options="user" class="mb-4"></v-select>
+
+            <field-messages name="user_id" show="$invalid && $submitted" class="text-danger">
+              <small class="form-text text-success">Looks good!</small>
+              <small class="form-text text-danger" slot="required">Username is a required field</small>
+            </field-messages>
+            </validate>
+          </div>
+        </div>
+
+        <div class="form-row mt-4">
+          <div class="col-md">
+            <button type="submit" class="btn btn-primary">Submit</button>
+
+            <button type="reset" class="btn btn-secondary" @click="reset">Reset</button>
+          </div>
+        </div>
+
+      </vue-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    axios.get('api/siswa/' + this.$route.params.id + '/edit')
+      .then(response => {
+        if (response.data.status == true) {
+          this.model.old_user_id      = response.data.siswa.user_id;
+          this.model.user             = response.data.user;
+          this.model.nomor_un         = response.data.siswa.nomor_un;
+          this.model.old_nomor_un     = response.data.siswa.nomor_un;
+          this.model.nik              = response.data.siswa.nik;
+          this.model.old_nik          = response.data.siswa.nik;
+          this.model.nama_siswa       = response.data.siswa.nama_siswa;
+          this.model.alamat_kk        = response.data.siswa.alamat_kk;
+          this.model.tempat_lahir     = response.data.siswa.tempat_lahir;
+          this.model.tgl_lahir        = response.data.siswa.tgl_lahir;
+          this.model.jenis_kelamin    = response.data.siswa.jenis_kelamin;
+          this.model.agama            = response.data.siswa.agama;
+          this.model.nisn             = response.data.siswa.nisn;
+          this.model.tahun_lulus      = response.data.siswa.tahun_lulus;
+        } else {
+          alert('Failed');
+        }
+      })
+      .catch(function(response) {
+        alert('Break');
+        window.location.href = '#/admin/siswa';
+      }),
+
+      axios.get('api/siswa/create')
+      .then(response => {
+          if(response.data.user_special == true){
+            response.data.user.forEach(user_element => {
+              this.user.push(user_element);
+            });
+          }else{
+            this.user.push(response.data.user);
+          }
+      })
+      .catch(function(response) {
+        alert('Break');
+        window.location = '#/admin/siswa';
+      })
+  },
+  data() {
+    return {
+      jenis_kelamin: [
+        {id: 1, label: 'Laki-laki'},
+        {id: 2, label: 'Perempuan'}
+      ],
+      selectedJenisKelamin: {id: "-", label: 'Pilih Salah Satu'},
+
+      agama: [
+        {id: 1, label: 'Islam'},
+        {id: 2, label: 'Kristen Protestan'},
+        {id: 3, label: 'Kristen Katolik'},
+        {id: 4, label: 'Hindu'},
+        {id: 5, label: 'Buddha'},
+        {id: 6, label: 'Khonghucu'}
+      ],
+      selectedAgama: {id: "-", label: 'Pilih Salah Satu'},
+      state: {},
+      model: {
+        label: "",
+        description: "",
+        user: "",
+        nomor_un: "",
+        nik: "",
+        nama_siswa: "",
+        alamat_kk: "",
+        tempat_lahir: "",
+        tgl_lahir: "",
+        jenis_kelamin: "",
+        agama: "",
+        nisn: "",
+        tahun_lulus: "",
+        old_user_id: "",
+        old_nomor_un: "",
+        old_nik: "",
+        old_nisn: "",
+      },
+      user: []
+    }
+  },
+  methods: {
+    onSubmit: function() {
+      let app = this;
+
+      if (this.state.$invalid) {
+        return;
+      } else {
+        axios.put('api/siswa/' + this.$route.params.id, {
+            user_id:        this.model.user.id,
+            old_user_id:    this.model.old_user_id,
+            nomor_un:       this.model.nomor_un,
+            old_nomor_un:   this.model.old_nomor_un,
+            nik:            this.model.nik,
+            old_nik:        this.model.old_nik,
+            nama_siswa:     this.model.nama_siswa,
+            alamat_kk:      this.model.alamat_kk,
+            tempat_lahir:   this.model.tempat_lahir,
+            tgl_lahir:      this.model.tgl_lahir,
+            jenis_kelamin:  this.model.jenis_kelamin.id,
+            agama:          this.model.agama.id,
+            nisn:           this.model.nisn,
+            tahun_lulus:    this.model.tahun_lulus,
+
+
+          })
+          .then(response => {
+            if (response.data.status == true) {
+              if(response.data.message == 'success'){
+                alert(response.data.message);
+                app.back();
+              }else{
+                alert(response.data.message);
+              }
+            } else {
+              alert(response.data.message);
+            }
+          })
+          .catch(function(response) {
+            alert('Break ' + response.data.message);
+          });
+      }
+    },
+    reset() {
+      axios.get('api/siswa/' + this.$route.params.id + '/edit')
+        .then(response => {
+          if (response.data.status == true) {
+            this.model.label = response.data.siswa.label;
+            this.model.description = response.data.siswa.description;
+          } else {
+            alert('Failed');
+          }
+        })
+        .catch(function(response) {
+          alert('Break ');
+        });
+    },
+    back() {
+      window.location = '#/admin/siswa';
+    }
+  }
+}
+</script>
